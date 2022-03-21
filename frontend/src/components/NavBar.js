b0c22a5f583ef9060cce6b17cff2fb191481a9ca
SoupyOfficial <jscampbell21@outlook.com>
2022-03-20 22:27:27 -0400

    NavBar Update

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,22 +1,28 @@
 import React, { useRef } from 'react'
-import { Button, Col, Container, Form, Nav } from 'react-bootstrap'
+import { Button, Grid, Col, Container, Form, Navbar} from 'react-bootstrap'
 
 function NavBar() {
 
   const SearchRef = useRef()
   return (
     <>
-    <div className="text-center" style={{ backgroundColor: "#484848", color: "white", width:'100vw'}}>
-        <Container fluid className='row' style={{textAlign: 'center'}}>
-            <div className='col'><h1 style={{ color: "blue"}}>Asobi</h1></div>
-            <Nav.Link className='col' href='/home'>Home</Nav.Link>
-            <Nav.Link className='col' href='/profile'>Profile</Nav.Link>
-            <Nav.Link className='col' href='/discover'>Discover</Nav.Link>
-            <Nav.Link className='col' href='/movie'>Movie Page</Nav.Link>
-            <Button className="col" style={{ margin: 5, padding: 5 }} href='/' >Login</Button>
-            <Button className="col" style={{ margin: 5, padding: 5 }} href='/register'>Sign Up</Button>
-                
-        </Container>
+    <div style={{ backgroundColor: "#484848", color: "white"}}>
+        <Navbar fluid>
+                <Navbar.Brand href='/home'>
+                    <h1 style={{ color: "blue"}}>Asobi</h1>
+                </Navbar.Brand>
+            
+            
+                <Navbar.Brand style={{ color: "white"}} href='/profile'>Profile</Navbar.Brand>
+            
+                <Navbar.Brand style={{ color: "white"}} href='/discover'>Discover</Navbar.Brand>
+
+            <Navbar.Brand style={{ color: "white"}} href='/movie'>Movie Page</Navbar.Brand>
+            <Col md={{offset: 3}}>
+                <Button className='position-sticky top-0 end-0' style={{ margin: 5, padding: 5 }} href='/' >Login</Button>
+                <Button className='position-sticky top-0 end-0' style={{ margin: 5, padding: 5 }} href='/register'>Sign Up</Button>
+            </Col>
+        </Navbar>
     </div>
     
     </>
