8eed744d8763cb364dafa003fc311d8a6db141f3
cjsodl926 <christian_sodl01@outlook.com>
2022-04-03 20:20:15 -0400

    ProfilePage

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -2,6 +2,45 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 
 export default function Profile() {
+
+  var _ud = localStorage.getItem('user_data');
+  var ud = JSON.parse(_ud);
+  var userId = ud.id;    
+  var firstName = ud.firstName;
+  var lastName = ud.lastName;
+  console.log(firstName);
+
+  var obj = {ID:userId};
+  var js = JSON.stringify(obj);
+  
+  const app_name = 'asobi-1'
+  function buildPath(route)
+  {
+      if (process.env.NODE_ENV === 'production') 
+      {
+          return 'https://' + app_name +  '.herokuapp.com/' + route;
+      }
+      else
+      {        
+          return 'http://localhost:5000/' + route;
+      }
+  }
+
+  try
+  {
+      const response = fetch(buildPath('api/loadprofile'),
+      {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+      
+      var txt = response.text();
+      var res = JSON.parse(txt);
+      console.log(res)
+      
+  }
+  catch(e)
+  {
+      console.log(e.toString());
+  }
+
   return (
     <>
     <NavBar/>
@@ -20,7 +59,7 @@ export default function Profile() {
           <div class="container-fluid d-flex align-items-center justify-content-center">
             <div class="row justify-content-center">
               <div class="col-lg-100 col-md-20">
-                <h1 class="display-2 text-white">Hello, User</h1>
+                <h1 class="display-2 text-white">Hello, {firstName}</h1>
                 <p class="text-white mt-0 mb-5">Welcome to Your Profile</p>
                 <a href="#!" class="btn btn-info">Edit Profile</a>
               </div>
@@ -32,7 +71,7 @@ export default function Profile() {
           <div class="col-sm-6 mb-3">
             <div class="card h-100 border-0 bg-transparent">
               <div class="card-body" id="card1">
-                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Friends/Favorites/Watchlist</i></h6>
+                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Watchlist</i></h6>
                 <div class="p-4">
                   <a href="#" class="text-underline-hover">Add</a>
                 </div>
@@ -42,7 +81,7 @@ export default function Profile() {
           <div class="col-sm-6 mb-3">
             <div class="card h-100 border-0 bg-transparent">
               <div class="card-body" id="card2">
-                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Friends/Favorites/Watchlist</i></h6>      
+                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">My Top Rated</i></h6>      
                 <div class="p-4">
                   <a href="#" class="text-underline-hover">Add</a>
                 </div>
@@ -50,8 +89,6 @@ export default function Profile() {
             </div>
           </div>
         </div>
-
-        <a href="#!" class="btn btn-info">Log Out</a>
 
       </div>
     </>
