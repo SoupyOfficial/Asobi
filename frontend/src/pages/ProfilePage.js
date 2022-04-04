4d6b398fd34a0d2ae89f8f56931d5046e3f576f7
cjsodl926 <christian_sodl01@outlook.com>
2022-04-03 20:02:12 -0400

    ProfilePage

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -7,41 +7,52 @@ export default function Profile() {
     <NavBar/>
       <div className="primaryBackground">
 
-        <div class="container">
-          <div class="main-body">
-              <div class="card">
-                  <div class="card-body">
-                      <div class="d-flex flex-column align-items-center text-center">
-                          //image here
-                          <div class="mt-3">
-                              //name here
-                              //information here
-                              <button class="btn btn-primary">Edit</button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-md-8">
-                  <div class="card mb-3">
-                      <div class="card-body">
-                          <div class="row">
-                              <div class="col-sm-3">
-                                  <h6 class="mb-0">Top Films</h6>
-                              </div>
-                          </div>
-                          <button class="btn btn-primary">Edit</button>
-                      </div>
-                  </div>
-              </div>
+        <div class="container-fluid d-flex align-items-center justify-content-center">
+          <div class="picture-container">
+            <div class="picture">
+              <img src="https://militaryhealthinstitute.org/wp-content/uploads/sites/37/2021/08/blank-profile-picture-png.png" class="picture-src" id="picturePreview" title=""></img>
+            </div>
           </div>
-      </div>
-
-      <div class="container mt-3">
-        <div class="mt-4 p-5 bg-primary text-white rounded">
-          <h1>My Movie List</h1>
         </div>
-      </div>
+
+        <div class="header pb-6 pt-4 pt-lg-4 d-flex align-items-center">
+        <span class="mask bg-gradient-default opacity-8"></span>
+          <div class="container-fluid d-flex align-items-center justify-content-center">
+            <div class="row justify-content-center">
+              <div class="col-lg-100 col-md-20">
+                <h1 class="display-2 text-white">Hello, User</h1>
+                <p class="text-white mt-0 mb-5">Welcome to Your Profile</p>
+                <a href="#!" class="btn btn-info">Edit Profile</a>
+              </div>
+            </div>
+          </div>
+        </div>
       
+        <div class="row gutters-sm">
+          <div class="col-sm-6 mb-3">
+            <div class="card h-100 border-0 bg-transparent">
+              <div class="card-body" id="card1">
+                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Friends/Favorites/Watchlist</i></h6>
+                <div class="p-4">
+                  <a href="#" class="text-underline-hover">Add</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 mb-3">
+            <div class="card h-100 border-0 bg-transparent">
+              <div class="card-body" id="card2">
+                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Friends/Favorites/Watchlist</i></h6>      
+                <div class="p-4">
+                  <a href="#" class="text-underline-hover">Add</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <a href="#!" class="btn btn-info">Log Out</a>
+
       </div>
     </>
   )
