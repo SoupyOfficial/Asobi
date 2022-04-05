7d1af5969cfcebce8b7d709b78fc4ad103625bf3
jnunez0319 <98064227+jnunez0319@users.noreply.github.com>
2022-04-04 23:02:44 -0400

    Update NavBar.js
    More controllable nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,25 +11,133 @@ function NavBar() {
 
   return (
     <>
-    <div className='NavBackground'>
-        <Navbar fluid="true">
-                <Navbar.Brand href='/home'>
-                    <img src={Asobi} alt={PageTitle} style={{ height:"3rem", width: "10rem"}}/>
-                </Navbar.Brand>            
-                <Navbar.Brand style={{ color: "white"}} href='/profile'>Profile</Navbar.Brand>
-            
-                <Navbar.Brand style={{ color: "white"}} href='/discover'>Discover</Navbar.Brand>
+    <head>
+      <link rel='stylesheet' href='NavBar.css'></link>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins"></link>
+    </head>
 
-                <Navbar.Brand style={{ color: "white"}} href='/movie'>Movie Page</Navbar.Brand>
+    <body>
+      <nav style=
+        {{ 
+          display:"flex",
+          flexDirection: "row",
+          alignItems:"center",
+          padding:"5px 10px",
+          
+          //alignSelf: "stretch",
+          //justifyContent:"left", 
+          
+          minHeight:"8vh",
+          backgroundColor: "#484848",
+          fontFamily:"Poppins, sans-serif",
+          fontSize: "28px"
+        }}>
 
-                <Navbar.Brand style={{ color: "white"}} href='/search'>Search Page</Navbar.Brand>
+        <div class="logo">
+          <img src={Asobi} alt={PageTitle} style={{ height:"3rem", width: "10rem"}}/>
+        </div>
+
+        <ul class="navLinks" style=
+          {{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "baseline",
+            padding: "3px",
+
+            flex: "none",
+            flexGrow: "1",
+            margin: "0px 5px",
+          }}>
+
+          <li style=
+          {{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            padding: "0px",
+
+            flex: "none",
+            alignSelf: "stretch",
+            flexGrow: "0",
+            margin: "0px 3px",
+            }}> 
             
-            <div className="navbar-nav ms-auto">
+            <a href="/profile" style=
+            {{
+              flex: "none",
+              order: "0",
+              flexGrow: "0",
+              margin: "0px 5px",
+              
+              color:"#AAAAAA", 
+              textDecoration:"none", 
+              display:"block",
+              padding:"8px",
+            }}>
+              Home
+            </a>
+            
+            <a href="/search" style=
+            {{
+              flex: "none",
+              order: "1",
+              flexGrow: "0",
+              margin: "0px 5px",
+              
+              color:"#AAAAAA", 
+              textDecoration:"none", 
+              display:"block",
+              padding:"8px",
+            }}>
+              Search
+            </a>
+
+            <a href="/movie" style=
+            {{
+              flex: "none",
+              order: "2",
+              flexGrow: "0",
+              margin: "0px 5px",
+              
+              color:"white", 
+              textDecoration:"none", 
+              display:"block",
+              padding:"8px",
+            }}>
+              Movies
+            </a>
+
+            <a href="/discover" style=
+            {{
+              flex: "none",
+              order: "3",
+              flexGrow: "0",
+              margin: "0px 5px",
+              
+              color:"white", 
+              textDecoration:"none", 
+              display:"block",
+              padding:"8px",
+            }}>
+              Discover
+            </a>
+
+          </li>
+
+        </ul>
+
+        <div className="navbar-nav ms-auto">
               {/* Check Login Status */}
             <div className='LoggedIn'>
                     {localStorage.getItem('user_data') ? <LoggedInName/> : 
                   
-                <div className="navbar-nav ms-auto">
+                <div className="navbar-nav ms-auto" style=
+                {{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  padding: "0px",
+                }}>
                     {/* Login Button */} 
                   <Dropdown align='end'>
                     <Dropdown.Toggle style={{ borderRadius:10, margin: 5 }}>Login</Dropdown.Toggle>
@@ -48,9 +156,10 @@ function NavBar() {
                 </div>
                     }
             </div>
-          </div>     
-        </Navbar>
-    </div>  
+          </div>  
+
+      </nav>
+    </body> 
     </>
   )
 }
