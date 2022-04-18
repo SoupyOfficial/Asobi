db716dd1ab24b2968a2d3d2161e9e8201037748a
jnunez0319 <98064227+jnunez0319@users.noreply.github.com>
2022-04-18 17:10:31 -0400

    fixed unresolved

---
+++ frontend/src/pages/Top250SeriesPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Top250SeriesUI from '../components/Top250SeriesUI'
+
+export default function Top250SeriesPage() {
+  return (
+    <div>
+      <NavBar/>
+      <Top250SeriesUI/>
+    </div>
+  )
+}
