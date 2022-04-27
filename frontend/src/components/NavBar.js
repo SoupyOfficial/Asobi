e93834cac7797120f7a243426bdd470754f05500
jnunez0319 <98064227+jnunez0319@users.noreply.github.com>
2022-04-27 18:07:23 -0400

    sticky nav

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -32,6 +32,8 @@ function NavBar() {
       <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
       </style>
+
+      
     </><nav>
 
         <div className="logo">
